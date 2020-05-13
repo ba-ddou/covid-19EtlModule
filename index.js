ed9a28d4396412f923aa125671ab8472ce6700b6
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-13 17:13:22 +0000

    group stats by country

---
+++ index.js
@@ -0,0 +1,36 @@
+const { groupByCountry, uniqueCountries } = require("./lib/groupByCountry");
+const _ = require("lodash");
+const data = require("./data");
+
+// groupByCountry(`${__dirname}\\db\\time_series_covid19_confirmed_global.csv`);
+
+// groupByCountry(`${__dirname}\\db\\time_series_covid19_deaths_global.csv`);
+
+// groupByCountry(`${__dirname}\\db\\time_series_covid19_recovered_global.csv`);
+
+async function run() {
+	let confirmedCountries = await uniqueCountries(
+		`${__dirname}\\db\\time_series_covid19_confirmed_global.csv`
+	);
+	console.log("confirmedCountries", confirmedCountries.length);
+	// await data.create("confirmedCountries", confirmedCountries);
+
+	let deathsCountries = await uniqueCountries(
+		`${__dirname}\\db\\time_series_covid19_deaths_global.csv`
+	);
+	console.log("deathsCountries", deathsCountries.length);
+	// await data.create("deathsCountries", deathsCountries);
+
+	let recoveredCountries = await uniqueCountries(
+		`${__dirname}\\db\\time_series_covid19_recovered_global.csv`
+	);
+	console.log("recoveredCountries", recoveredCountries.length);
+	// await data.create("recoveredCountries", recoveredCountries);
+
+	console.log("confirmedCountries, recoveredCountries");
+	console.log(_.xorWith(confirmedCountries, recoveredCountries));
+	console.log("deathsCountries, recoveredCountries");
+	console.log(_.xorWith(deathsCountries, recoveredCountries));
+}
+
+run();
