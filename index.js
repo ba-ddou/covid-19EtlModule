185f966a303bc58cd15b05a1235a1189446a7915
Abdelbasite Badou <abdelbassite.badou@gmail.com>
2020-05-21 23:29:50 +0000

    code comments and  a new property ( newActive)

--- index.js
+++ index.js
@@ -6,33 +6,49 @@ const _ = require("lodash");
 const data = require("./data");
 
 async function run() {
+	// get Ccountry grouped confirmed cases data
 	let confirmedData = await groupByCountry(
 		`${__dirname}\\db\\time_series_covid19_confirmed_global.csv`
 	);
+	// get Ccountry grouped recovered cases data
 	let recoveredData = await groupByCountry(
 		`${__dirname}\\db\\time_series_covid19_recovered_global.csv`
 	);
+	// get Ccountry grouped deaths data
 	let deadData = await groupByCountry(
 		`${__dirname}\\db\\time_series_covid19_deaths_global.csv`
 	);
-
+	// available countries list
 	let countries = getUniqueCountries(confirmedData);
 	console.log("run -> countries", countries);
+	// merge confirmed, recovered, and deaths data of the same country
+	// into the same object
 	let { aggregatedStats } = aggregateStats({
 		countries: countries,
 		confirmedData,
 		recoveredData,
 		deadData,
 	});
+	// get available dates list
 	let datesList = getDatesList(confirmedData);
 	let { dates, territories, stats } = normalize({
 		countries,
 		aggregatedStats,
 		datesList,
 	});
+
+	// There are many differences the country name spelling between
+	// the covid-19 data source and the data used to draw the global
+	// MAP component in the dashboard's UI.
+	// I attempted to sync some of the differences manually, and this functions
+	// fixes some of the country names in the countries data set to be the same
+	// as the ones used in the MAP component
 	territories = syncCountryNames({ territories });
+	// save dates data ( table ) as a json file
 	data.create("dates", dates);
+	// save coutries data ( table ) as a json file
 	data.create("territories", territories);
+	// save facts data ( table ) as a josn file
 	data.create("stats", stats);
 }
 
